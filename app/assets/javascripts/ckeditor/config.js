83c4c00c355ce3005d7fbb6a6864eca400becfa3
Shruti Sinha <shruti2809@gmail.com>
2015-08-04 11:46:14 +0530

    Slimming down the toolbar for richtext editor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,4 +1,4 @@
-/*
+﻿/*
 Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
@@ -11,25 +11,25 @@ CKEDITOR.editorConfig = function( config )
 
   /* Filebrowser routes */
   // The location of an external file browser, that should be launched when "Browse Server" button is pressed.
-  // config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
+  config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
 
   // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Flash dialog.
-  // config.filebrowserFlashBrowseUrl = "/ckeditor/attachment_files";
+  config.filebrowserFlashBrowseUrl = "/ckeditor/attachment_files";
 
   // The location of a script that handles file uploads in the Flash dialog.
-  // config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
+  config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
 
   // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Link tab of Image dialog.
-  // config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
+  config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
 
   // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Image dialog.
-  // config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
+  config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
 
   // The location of a script that handles file uploads in the Image dialog.
-  // config.filebrowserImageUploadUrl = "/ckeditor/pictures";
+  config.filebrowserImageUploadUrl = "/ckeditor/pictures";
 
   // The location of a script that handles file uploads.
-  // config.filebrowserUploadUrl = "/ckeditor/attachment_files";
+  config.filebrowserUploadUrl = "/ckeditor/attachment_files";
 
   config.allowedContent = true;
 
